71027b7a750fcb134c91b7feec89fbe2098379c4
Ben Russell <bprussell80@protonmail.com>
2022-02-16 12:03:12 -0800

    Change database name back to default

--- budget/settings.py
+++ budget/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'budget.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'example.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
